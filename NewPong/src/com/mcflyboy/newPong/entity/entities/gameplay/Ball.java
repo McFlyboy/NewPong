596827ff61fc634bef84cb045b945ea97c8f11bc
Henrik <hsn95@live.com>
2018-09-24 03:58:26 +0200

    Added a max-speed to the ball

--- NewPong/src/com/mcflyboy/newPong/entity/entities/gameplay/Ball.java
+++ NewPong/src/com/mcflyboy/newPong/entity/entities/gameplay/Ball.java
@@ -5,6 +5,7 @@ import com.mcflyboy.newPong.math.Vector2f;
 
 public class Ball extends GameEntity {
 	private static final float MIN_SPEED = 0.75f;
+	private static final float MAX_SPEED = 5f;
 	private static final float DECCELERATION = 0.2f;
 	private Vector2f acceleration;
 	public Ball() {
@@ -24,13 +25,25 @@ public class Ball extends GameEntity {
 	public void updateVelocity(float deltaTime) {
 		acceleration = super.getVelocity().getNegate().getMul(DECCELERATION);
 		super.getVelocity().add(acceleration.getMul(deltaTime));
-		if(super.getVelocity().getLength() <= MIN_SPEED) {
-			super.getVelocity().normalize();
-			super.getVelocity().mul(MIN_SPEED);
+		if(super.getVelocity().getLength() < MIN_SPEED) {
+			adjustSpeed(MIN_SPEED);
+		}
+		else if(super.getVelocity().getLength() > MAX_SPEED) {
+			adjustSpeed(MAX_SPEED);
 		}
 	}
 	public void reset(Vector2f position) {
 		super.setPosition(new Vector2f(position));
 		super.setVelocity(new Vector2f(-2f, -0.5f));
+	}
+	
+	/**
+	 * Adjusts the speed of the ball.
+	 * Will only work if the current speed is zero in value.
+	 * @param speed The ball's new speed
+	 */
+	private void adjustSpeed(float speed) {
+		super.getVelocity().normalize();
+		super.getVelocity().mul(speed);
 	}
 }
